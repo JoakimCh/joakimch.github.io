44a0e3407bb0d2dcab751628b836365441a6ff8e
Joakim L. Christiansen <christiansen.joakim@gmail.com>
2020-04-06 12:16:44 +0200

    Create index.html.js

---
+++ index.html.js
@@ -0,0 +1,6 @@
+const header = document.createElement('h1')
+header.innerText = 'Hello World!'
+document.body.appendChild(header)
+let tmp = document.createElement('p')
+tmp.innerText = 'Something will eventually be here. Some day... I guess!'
+document.body.appendChild(tmp)
